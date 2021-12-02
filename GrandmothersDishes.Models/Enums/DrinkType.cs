5d07f77820111769435e6aab05adde21497741c1
GerganaLp <77080812+GerganaLp@users.noreply.github.com>
2021-12-02 18:05:09 +0200

    Add files via upload

---
+++ GrandmothersDishes.Models/Enums/DrinkType.cs
@@ -0,0 +1,13 @@
+﻿namespace GrandmothersDishes.Models.Enums
+{
+    public enum DrinkType
+    {
+        Hot = 1,
+        Juice = 2,
+        NonAlocoholic = 3,
+        EnergyDrink = 4,
+        Alcohol = 5,
+        Cocktail = 6,
+        Beer = 7,
+    }
+}
---
+++ GrandmothersDishes.Models/Enums/EmployeeType.cs
@@ -0,0 +1,10 @@
+﻿namespace GrandmothersDishes.Models.Enums
+{
+    public enum EmployeeType
+    {
+        Chef = 1,
+        Seller = 2,
+        Manager = 3,
+        DeliveryGuy = 4,
+    }
+}
